beb7df7e42401122c8507f9463625de21f71934c
Yuya Saito <studiomohawk@gmail.com>
2015-02-11 23:47:29 +0900

    update delpoy script

--- _devel/bin/deploy.sh
+++ _devel/bin/deploy.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
 (git subtree split --prefix public -b gh-pages)
 (git push -f origin gh-pages:gh-pages)
 (git branch -D gh-pages)
+
+# Push source and build repos.
+git push origin master
+git subtree push --prefix=public git@github.com:enja/quote.git gh-pages
